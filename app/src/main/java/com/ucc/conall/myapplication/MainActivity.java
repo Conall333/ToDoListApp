cb6c6c782642f82065409c609b7acc5f62e09509
conall333 <conallmccarthy@hotmail.com>
2018-11-02 19:09:08 +0000

    v1

--- app/src/main/java/com/ucc/conall/myapplication/MainActivity.java
+++ app/src/main/java/com/ucc/conall/myapplication/MainActivity.java
@@ -41,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
         session = new SharedPrefs(getApplicationContext());
 
         if (session.isLoggedIn()){
+            
 
             try {
                 session.backgroundLoginCheck();
